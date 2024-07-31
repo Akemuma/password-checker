535bb6cd34d6997a6b3916bf6a447b84a6e0bff2
Akemuma <142207344+Akemuma@users.noreply.github.com>
2024-07-31 10:36:40 +0300

    Update password_checkers.py

--- password_checkers.py
+++ password_checkers.py
@@ -3,4 +3,10 @@ import math
 
 def calculate_entropy(password):
      pool_size = 0
-     
+         if any(c.islower() for c in password):
+        pool_size += 26
+    if any(c.isupper() for c in password):
+        pool_size += 26
+    if any(c.isdigit() for c in password):
+        pool_size += 10
+
